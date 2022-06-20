cc6213169c3d274be309b3e1bba2509b6b842ae7
Harshit <khk9876kumarr@gmail.com>
2022-06-20 10:35:03 +0530

    cleared boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        <h1 style={{ textAlign: "center" }}>CAMPUS OLX</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
