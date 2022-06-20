f803b30e6000e46d2a3ea52549d9735816f52035
Harshit <khk9876kumarr@gmail.com>
2022-06-20 10:46:12 +0530

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ const App = () => {
     <>
       <div className="app">
         <h1 style={{ textAlign: "center" }}>CAMPUS OLX</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nisi
+          magni corporis odio doloremque, quaerat sequi vero optio unde commodi
+          autem exercitationem labore rerum facilis tempore odit dolores ipsam,
+          reprehenderit libero veniam, ab nesciunt quam cum. Quasi dolorem,
+          commodi hic inventore, quod non corrupti quo vero at voluptatum
+          recusandae alias.
+        </p>
       </div>
     </>
   );
